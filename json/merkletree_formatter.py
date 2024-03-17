1bde878018a8df67bc766bb03888ff70795a5e5d
Amir <pascalazubi@gmail.com>
2024-03-17 15:03:11 +0100

    Optimized the script

--- json/merkletree_formatter.py
+++ json/merkletree_formatter.py
@@ -5,10 +5,13 @@ with open('FILENAME.json', 'r') as file:
     original_data = json.load(file)
 
 # Transform the data
-transformed_data = [
-    {"address": address, "amount": details["amount"]}
-    for address, details in original_data.items()
-]
+transformed_data = []
+for item in original_data:
+    for address, details in item.items():
+        transformed_data.append({
+            "address": address,
+            "amount": int(details["amount"], 16)  # Convert hex to decimal
+        })
 
 # Write the transformed data back to the file
 with open('FILENAME.json', 'w') as file:
